41c4425b08c79f9bd8664be5e493ade7c6cea004
PaddyLH <patrickharmer5@gmail.com>
2022-10-28 17:19:03 +0100

    Update

--- robot.py
+++ robot.py
@@ -1,4 +1,8 @@
 
+# This is the main file for the robot
+# The main operations are run here for clarity
+
+
 import math
 import time
 
@@ -11,19 +15,20 @@ import movement
 import calibration
 import music
 
-Rich = constants.Richard2
+Jeremy = constants.Richard2
 Bot = constants.Data
 
 Bot.x, Bot.y, Bot.bearing = tracking.get_position()
 Bot.target_x, Bot.target_y, Bot.target_bearing = 0, 0, 0
-Bot.home_base = Rich.zone()
+Bot.home_base = Jeremy.zone()
 
 movement.stop()
 music.wake_up()
 
-Rich.wait_start()
+Jeremy.wait_start()
 
-#calibration.forward_calib_marker()
+calibration.forward_calib_marker()
+calibration.forward_calib(2)
 
 movement.drive_forward(2)
 movement.drive_left(2)
