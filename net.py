16585d723ba10b84b429b2335411051c740b88f3
Paddy Harmer <patrickharmer5@gmail.com>
2022-11-27 09:47:59 +0000

    Updated Neural Network with 100% Accuracy

--- net.py
+++ net.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 import time
 import math
 
-# currently not accurate and working neural network 
-# There is a working model that I will upload here soon
-
 file = open("out.txt")
 text = file.read()
 file.close()
@@ -33,80 +30,150 @@ for item in t:
         else: c+= item[i]
     if c != "": v.append(c)
 
-    v[0] = (math.degrees(float(v[0])) % 360) / 360
+    v[0] = float(v[0])
     if (v[0] < 0): print(v[0])
-    v[1] = (math.degrees(float(v[1])) % 360) / 360
+    v[1] = float(v[1])
     if (v[1] < 0): print(v[1])
-    v[2] = (math.degrees(float(v[2])) % 360) / 360
+    v[2] = float(v[2])
     if (v[2] < 0): print(v[2])
-    v[3] = float(int(v[3]) / 3000) 
+    v[3] = int(v[3]) / 1000
     v[4] = int(v[4])
 
+    print(v)
+
     iss.append([v[0], v[1], v[2], v[3]])
-    if v[4] == 1: oss.append([1])
-    else: oss.append([0])
+    oss.append(v[4])
 
 print("DONE")
 #print(iss)
 #print([oss])
 
 class NeuralNetwork:
+    def __init__(self, learning_rate):
+        self.weights = np.array([-161.850899, 14.8545939, 0.115096154, -2.06175901])
+        self.bias = np.array(28.86585632087976)
+        self.learning_rate = learning_rate
 
-    # intialize variables in class
-    def __init__(self, inputs, outputs):
-        self.inputs  = inputs
-        self.outputs = outputs
-        # initialize weights as .50 for simplicity
-        self.weights = np.array([[.50], [.50], [.50], [.50]])
-        self.error_history = []
-        self.epoch_list = []
-
-    #activation function ==> S(x) = 1/1+e^(-x)
-    def sigmoid(self, x, deriv=False):
-        if deriv == True:
-            return x * (1 - x)
+    def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-    # data will flow through the neural network.
-    def feed_forward(self):
-        self.hidden = self.sigmoid(np.dot(self.inputs, self.weights))
+    def _sigmoid_deriv(self, x):
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
 
-    # going backwards through the network to update weights
-    def backpropagation(self):
-        self.error  = self.outputs - self.hidden
-        delta = self.error * self.sigmoid(self.hidden, deriv=True)
-        self.weights += np.dot(self.inputs.T, delta)
-
-    # train the neural net for 25,000 iterations
-    def train(self, epochs=25000):
-        for epoch in range(epochs):
-            self.feed_forward()
-            self.backpropagation()    
-            self.error_history.append(np.average(np.abs(self.error)))
-            self.epoch_list.append(epoch)
-
-    def predict(self, new_input):
-        prediction = self.sigmoid(np.dot(new_input, self.weights))
+    def predict(self, input_vector):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
         return prediction
 
-NN = NeuralNetwork(np.array(iss), np.array(oss))
-NN.train()
+    def _compute_gradients(self, input_vector, target):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+
+        derror_dprediction = 2 * (prediction - target)
+        dprediction_dlayer1 = self._sigmoid_deriv(layer_1)
+        dlayer1_dbias = 1
+        dlayer1_dweights = (0 * self.weights) + (1 * input_vector)
+
+        derror_dbias = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+        )
+        derror_dweights = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
+        )
+
+        return derror_dbias, derror_dweights
+
+    def _update_parameters(self, derror_dbias, derror_dweights):
+        self.bias = self.bias - (derror_dbias * self.learning_rate)
+        self.weights = self.weights - (
+            derror_dweights * self.learning_rate
+        )
+    
+    def train(self, input_vectors, targets, iterations):
+        cumulative_errors = []
+        for current_iteration in range(iterations):
+            # Pick a data instance at random
+            random_data_index = np.random.randint(len(input_vectors))
+
+            input_vector = input_vectors[random_data_index]
+            target = targets[random_data_index]
+
+            # Compute the gradients and update the weights
+            derror_dbias, derror_dweights = self._compute_gradients(
+                input_vector, target
+            )
+
+            self._update_parameters(derror_dbias, derror_dweights)
+
+            # Measure the cumulative error for all the instances
+            if current_iteration % 100 == 0:
+                cumulative_error = 0
+                # Loop through all the instances to measure the error
+                for data_instance_index in range(len(input_vectors)):
+                    data_point = input_vectors[data_instance_index]
+                    target = targets[data_instance_index]
+
+                    prediction = self.predict(data_point)
+                    error = np.square(prediction - target)
+
+                    cumulative_error = cumulative_error + error
+                cumulative_errors.append(cumulative_error)
+
+            if current_iteration % 10000 == 0:
+                print(current_iteration / iterations * 100,"% Done")
+
+        return cumulative_errors
+
+NN = NeuralNetwork(0.1)
+ef = NN.train(np.array(iss), np.array(oss), 100000)
+
+plt.plot(ef)
+plt.xlabel("Iterations")
+plt.ylabel("Error")
+plt.show()
 
 correct = 0
 for i in range(len(iss)):
     inp = iss[i]
     out = oss[i]
     output = NN.predict(np.array(inp))
-    print("output", output)
     if output >= 0.5: output = 1
     else: output = 0
-    if (out[0] == output):correct += 1
+    if (out == output): correct += 1
 
 print(correct / len(iss) * 100)
+print(NN.weights, NN.bias)
 
+def small_predictor(data):
+    weights = [-161.850899, 14.8545939, 0.115096154, -2.06175901]
+    bias = 28.86585632087976
+    g = 1 / (1 + np.exp(-1 * (np.dot(data, weights) + bias)))
+    if g > 0.5: return 1
+    return 0
 
-plt.figure(figsize=(15,5))
-plt.plot(NN.epoch_list, NN.error_history)
-plt.xlabel('Epoch')
-plt.ylabel('Error')
-plt.show()
+print(small_predictor(iss[-1]),oss[-1])
+
+def index(x,y):
+    i = (y / 3) * 400
+    j = ((x + 0.3) / 0.6) * 400
+    return i, j
+
+data = []
+for i in range(400):
+    data.append([])
+    for j in range(400):
+        x = ((j / 100) * 0.6) - 0.3
+        z = ((i / 100) * 0.6) - 0.3
+        y = (i / 100) * 3
+
+        data[-1].append(small_predictor([0,x,0,y]))
+
+c = 0
+for d in iss:
+    a,b = index(d[1], d[3])
+    if 0 <= a < 400 and 0 <= b < 400:
+        data[int(a)][int(b)] = oss[c] + 4
+    c += 1
+
